b1c44477f810f0595ff7e4aeae2b6d4c983c4581
Mohammad Ranjbar <mj.ranjbar.94@gmail.com>
2020-01-24 17:15:57 +0330

    initial project

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,43 @@
+
+<header  style="font-size: large">
+    <nav class=" navbar  navbar-dark fixed-top bg-dark">
+
+
+
+                <ul class="nav justify-content-end">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/login">ورود</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/register">ثبت نام</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">لیست موضوعی</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">انجمن</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">اخبار</a>
+                    </li>
+                </ul>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">انجمن پژوهشگران هنر </a>
+                </li>
+
+            </ul>
+
+        <ul class="nav ">
+            <li class="nav-item">
+                <form class="form-inline mt-2 mt-md-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="جستجو ..." aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">جستجو</button>
+                </form>
+
+            </li>
+
+        </ul>
+
+    </nav>
+</header>
