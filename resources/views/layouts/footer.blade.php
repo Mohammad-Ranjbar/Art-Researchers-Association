3df73709a072c16217d9facbdfa6bd4e21ab8f65
Mohammad Ranjbar <mj.ranjbar.94@gmail.com>
2020-02-04 08:02:37 +0330

    change footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,41 +1,13 @@
 <!-- Footer -->
 <footer class="page-footer font-small unique-color-dark" dir="rtl">
 
-    <div style="background-color: #6351ce;">
+    <div style="background-color: #1b1e21;">
         <div class="container">
 
             <!-- Grid row-->
             <div class="row py-4 d-flex align-items-center">
 
-                <!-- Grid column -->
-                <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
-                    <h6 class="mb-0">Get connected with us on social networks!</h6>
-                </div>
-                <!-- Grid column -->
-
-                <!-- Grid column -->
                 <div class="col-md-6 col-lg-7 text-center text-md-right">
-
-                    <!-- Facebook -->
-                    <a class="fb-ic">
-                        <i class="fab fa-facebook-f white-text mr-4"> </i>
-                    </a>
-                    <!-- Twitter -->
-                    <a class="tw-ic">
-                        <i class="fab fa-twitter white-text mr-4"> </i>
-                    </a>
-                    <!-- Google +-->
-                    <a class="gplus-ic">
-                        <i class="fab fa-google-plus-g white-text mr-4"> </i>
-                    </a>
-                    <!--Linkedin -->
-                    <a class="li-ic">
-                        <i class="fab fa-linkedin-in white-text mr-4"> </i>
-                    </a>
-                    <!--Instagram-->
-                    <a class="ins-ic">
-                        <i class="fab fa-instagram white-text"> </i>
-                    </a>
 
                 </div>
                 <!-- Grid column -->
@@ -56,11 +28,13 @@
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4" align="right">
 
                 <!-- Content -->
-                <h6 class="text-uppercase font-weight-bold">Company name</h6>
+                <h6 class=" font-weight-bold">پژوهشگران هنر</h6>
                 <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-                <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                <p>
+                    Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
                     consectetur
-                    adipisicing elit.</p>
+                    adipisicing elit.
+                </p>
 
             </div>
             <!-- Grid column -->
@@ -69,7 +43,7 @@
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4" align="right">
 
                 <!-- Links -->
-                <h6 class="text-uppercase font-weight-bold">Products</h6>
+                <h6 class="text-uppercase font-weight-bold">محصولات</h6>
                 <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
                 <p>
                     <a href="#!">MDBootstrap</a>
@@ -91,7 +65,7 @@
             <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4" align="right">
 
                 <!-- Links -->
-                <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+                <h6 class="text-uppercase font-weight-bold">لینک های مفید</h6>
                 <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
                 <p>
                     <a href="#!">Your Account</a>
@@ -113,16 +87,10 @@
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4" align="right">
 
                 <!-- Links -->
-                <h6 class="text-uppercase font-weight-bold" >تماس با ما</h6>
+                <h6 class="text-uppercase font-weight-bold">تماس با ما</h6>
                 <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-                <p>
-                    <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                <p>
-                    <i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                <p>
-                    <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                <p>
-                    <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+
+                <p>info@example.com</p>
 
             </div>
             <!-- Grid column -->
